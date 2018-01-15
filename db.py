2f1d919a74e76d709c1b7990572317466044c17f
el_carbon <valery@spentapp.com>
2018-01-15 16:46:22 +0300

    Fix sign in

--- db.py
+++ db.py
@@ -72,8 +72,6 @@ def auth_user(name, password):
     p = yield executor.submit(
         bcrypt.hashpw, tornado.escape.utf8(password),
         tornado.escape.utf8(hashed_password))
-    hashed_password = p[0] if hashed_password else None
-
     if hashed_password == p:
         logging.info('db.get_user: {}'.format(name))
         return name
